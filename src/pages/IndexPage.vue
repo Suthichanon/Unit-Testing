ebbc8a8584c96315e31d42baea7f06edc5645fe3
Suthichanon <120318528+Suthichanon@users.noreply.github.com>
2024-01-26 10:20:27 +0700

    jest-test-header

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<!-- IndexPage.vue -->
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'Vue is not awesome.',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
